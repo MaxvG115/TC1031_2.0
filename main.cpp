6e2c6beaa3c5c72041e00306fc91eb533e04ec84
Vicente Viera <plantedcoot706@gmail.com>
2021-10-06 15:11:12 -0500

    agrego funcion para leer el archivo y guardar los datos en un vector. Modifico git ignore

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <list>
+#include <vector>
 
 using namespace std;
 
@@ -46,3 +49,44 @@ struct record{
           return 0;
         }
 };
+
+//funcion para leer archivo y guardar los datos en un vector
+void readFile(vector<record>& Vector){
+    record line;
+    string aux;
+    ifstream file;//objeto para leer archivo
+    file.open("bitacora.txt");
+
+    if(file.fail()){
+        exit(0);
+    }else{
+        //ciclo para identificar el significado de los datos de cada registro
+        while (!file.eof()){
+            for(int i=0;i<3;i++){
+                 getline(file, aux,' ');
+                    line.date+=aux+' ';
+            }
+            getline(file, line.IPqrt1, '.');
+            getline(file, line.IPqrt2, '.');
+            getline(file, line.IPqrt3, '.');
+            getline(file, line.IPqrt4, ':');
+            getline(file, line.port, ' ');
+            getline(file, line.status);
+
+            //line.month=mon2num(line.month);
+            Vector.push_back(line);
+            line.date="";
+        }
+    }
+    file.close();
+}
+
+int main(){
+    //creamos variables iniciales
+    vector<record>* vec= new vector<record>();
+    list<int> li;
+
+    readFile(*vec);
+    cout<<vec->at(0).IPqrt1<<vec->at(0).IPqrt2<<vec->at(0).IPqrt3<<vec->at(0).IPqrt4<<vec->at(0).port;
+    return 0;
+}
